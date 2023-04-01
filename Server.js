dcf900d7283074975c49f1d0ca49bdab70f9e43c
subhashishshah <subhashish@ismt.edu.np>
2023-04-01 12:24:24 +0545

    first commit

--- Server.js
+++ Server.js
@@ -1,26 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const bodyParser = require("body-parser");
+// Database connection
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://sudeep:Sudiptamang1@cluster0.iyenujk.mongodb.net/assignmentACS?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
+// define the user schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+// create the user model
+const User = mongoose.model("User", userSchema);
+app.use(bodyParser.urlencoded({ extended: false }));
 // Set the view engine to use Handlebars
-app.set('view engine', 'hbs');
-
+app.set("view engine", "hbs");
 // Set the path to your views folder
-app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'static')));
+app.set("views", __dirname + "/views");
+app.use(express.static(path.join(__dirname, "static")));
 
 // Define your routes
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Login Page' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "Login Page" });
 });
-app.get('/register', (req, res) => {
-    res.render('register', { title: 'Register page' });
-  });
-  app.get('/forgetpass', (req, res) => {
-    res.render('forgetpass', { title: 'Forget page' });
-  });
+app.get("/register", (req, res) => {
+  res.render("register", { title: "Register page" });
+});
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+  const student = await User.create({ ...req.body });
+  res.redirect("/");
+});
+app.get("/forgetpass", (req, res) => {
+  res.render("forgetpass", { title: "Forget page" });
+});
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
+  console.log("Server started on port 3000");
 });
